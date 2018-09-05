f660b2b7cbe59f7e22609cbece391cc51b7709a1
Adam Love <adam.love127@gmail.com>
2018-09-06 09:22:57 +1000

    Use environment variables

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
+    host : process.env.DB_HOST,
+    user : '',
     password : '',
-    database : 'smart-brain'
+    database : process.env.DB_NAME
   }
 });
 
